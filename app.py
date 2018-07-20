25760258c2c2d9d301285b4b7889197a5eb0a8c7
oakis <oakismen@gmail.com>
2018-07-20 14:51:32 +0200

    handle HTTPException

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 """ Mina Hållplatser API """
+# from urllib.error import HTTPError
 import datetime
+from werkzeug.exceptions import HTTPException
 import requests
 from flask import Flask, jsonify, request
 
@@ -15,21 +17,36 @@ def get_nearby_stops():
     """ GPS """
     APP.logger.info('get_nearby_stops():')
 
-    data = request.get_json()
-    latitude = data['latitude']
-    longitude = data['longitude']
-    access_token = request.headers['access_token']
+    try:
+        data = request.get_json()
+        latitude = data['latitude']
+        longitude = data['longitude']
+        access_token = request.headers['access_token']
 
-    url = 'https://api.vasttrafik.se/bin/rest.exe/v2/location.nearbystops?originCoordLat='\
-          + str(latitude) + '&originCoordLong=' + str(longitude) + '&format=json'
-    headers = {'Authorization': 'Bearer ' + access_token}
-    req = requests.get(url, headers=headers)
-    response = req.json()['LocationList']['StopLocation']
-    filtered = [item for item in response if 'track' not in item]
+        url = 'https://api.vasttrafik.se/bin/rest.exe/v2/location.nearbystops?originCoordLat='\
+            + str(latitude) + '&originCoordLong=' + str(longitude) + '&format=json'
+        headers = {'Authorization': 'Bearer ' + access_token}
+        req = requests.get(url, headers=headers)
+        if req.status_code != 200:
+            raise HTTPException(description=req.json())
+        req.raise_for_status()
+        response = req.json()['LocationList']['StopLocation']
+        filtered = [item for item in response if 'track' not in item]
 
-    return jsonify({
-        'data': filtered,
-        'timestamp': datetime.date.today()
+        return jsonify({
+            'data': filtered,
+            'timestamp': datetime.date.today()
+        })
+    except HTTPException as err:
+        return make_error(500, err.description)
+
+def make_error(status_code, message):
+    """ Function for returning errors easily """
+    response = jsonify({
+        'status': status_code,
+        'message': message,
     })
+    response.status_code = status_code
+    return response
 
 APP.run(port=5000, debug=True)
